7fd241ac2030f06ea439ee993c63c9f1054c30b5
gihan-r <kavimcg03@gmail.com>
2025-03-24 09:12:38 +0530

    updated the SupermarketProfitController

--- HopeBridge Backend Prototype/src/main/java/com/example/hopebridge/controllers/SupermarketProfitController.java
+++ HopeBridge Backend Prototype/src/main/java/com/example/hopebridge/controllers/SupermarketProfitController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.*;
 public class SupermarketProfitController {
 
     private final SupermarketProfitService supermarketProfitService;
+//Constructor to inject SupermarketProfitService.
 
     public SupermarketProfitController(SupermarketProfitService supermarketProfitService) {
         this.supermarketProfitService = supermarketProfitService;
     }
 
+//Endpoint to retrieve the profit of a specific supermarket.
     @GetMapping("/{supermarketId}/profit")
     public double getSupermarketProfit(@PathVariable Long supermarketId) {
         return supermarketProfitService.calculateProfit(supermarketId);
