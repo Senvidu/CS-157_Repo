49a4375dd5f00b15899ce0c09c2e52b7468e610c
gihan-r <kavimcg03@gmail.com>
2025-03-24 09:16:34 +0530

    updated the VoucherController class

--- HopeBridge Backend Prototype/src/main/java/com/example/hopebridge/controllers/VoucherController.java
+++ HopeBridge Backend Prototype/src/main/java/com/example/hopebridge/controllers/VoucherController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//Provides an endpoint to create new vouchers
 @RestController
 @RequestMapping("/api/vouchers")
 public class VoucherController {
