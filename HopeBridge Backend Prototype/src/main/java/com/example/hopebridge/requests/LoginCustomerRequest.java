613ea1b565762646628826955833bc19aaa611ad
Naveen5811 <naveenthanusha8@gmail.com>
2026-03-23 12:02:51 +0530

    fix: remove Lombok from LoginCustomerRequest class

--- HopeBridge Backend Prototype/src/main/java/com/example/hopebridge/requests/LoginCustomerRequest.java
+++ HopeBridge Backend Prototype/src/main/java/com/example/hopebridge/requests/LoginCustomerRequest.java
@@ -1,13 +1,17 @@
 package com.example.hopebridge.requests;
 
-import lombok.*;
-
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class LoginCustomerRequest {
     private String username;
     private String password;
+
+    public LoginCustomerRequest() {}
+    public LoginCustomerRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
